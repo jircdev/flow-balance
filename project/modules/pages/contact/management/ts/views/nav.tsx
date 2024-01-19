0464e5818f20eaf515b372a959b9b3ded7609b78
gabojcb <gaboj0531@gmail.com>
2024-01-19 19:14:52 -0400

    Style: move folder home in pages and adjuts navigation of management contact pages

--- project/modules/pages/contact/management/ts/views/nav.tsx
+++ project/modules/pages/contact/management/ts/views/nav.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { IconButton } from "pragmate-ui/icons";
 import { BaseToolbar } from 'flow-balance/toolbar';
 import { Button } from 'pragmate-ui/components';
+import {routing} from "@beyond-js/kernel/routing";
 
 interface INavForm {
     onClick?: Function
 }
 
 export function NavForm({onClick}: INavForm) {
+
+    const handleRedirect = () => routing.pushState('/contact/list');
 
     const handleClick = () => {
         if(onClick) {
@@ -19,7 +22,7 @@ export function NavForm({onClick}: INavForm) {
             <BaseToolbar>
                 <div className="container__nav-form">
                     <section className="content-header">
-                        <IconButton className='lg'icon='close' />
+                        <IconButton className='xs' onClick={handleRedirect} icon='close' />
                         <h3 className="nav-form__h3">Create Contact</h3>
                     </section>
                     <Button variant="primary" onClick={handleClick}>Save</Button>
