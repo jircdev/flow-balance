abc0c4f8658235ad3f71f17f08c920ec78912d35
Julio Rodriguez <jircdev@gmail.com>
2024-02-06 22:18:38 -0300

    chore: basic home structure

--- project/modules/pages/home/ts/views/index.tsx
+++ project/modules/pages/home/ts/views/index.tsx
@@ -3,13 +3,25 @@ import { Icon } from 'pragmate-ui/icons';
 export /*bundle*/
 function View(): JSX.Element {
 	return (
-		<>
+		<main className='page-container'>
+			<div className='card'>
+				<section className='available-amount'>5000</section>
+			</div>
+
 			<ul className='main-home__menu'>
 				<li>
 					<Icon icon='home' />
-					Transactions
+					Registro
+				</li>
+				<li>
+					<Icon icon='home' />
+					Transferir
+				</li>
+				<li>
+					<Icon icon='users' />
+					Contactos
 				</li>
 			</ul>
-		</>
+		</main>
 	);
 }
