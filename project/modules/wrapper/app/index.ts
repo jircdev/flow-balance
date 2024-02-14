bda4a9fd77a10bf7697f87e3ed3571a7e31c459d
gabojcb <gaboj0531@gmail.com>
2024-02-13 20:51:00 -0400

    Chore: create wrapper module

---
+++ project/modules/wrapper/app/index.ts
@@ -0,0 +1,7 @@
+import { ReactiveModel } from '@beyond-js/reactive/model';
+
+class AppWrapper extends ReactiveModel<AppWrapper> {
+        
+}
+
+export /* bundle */ const appWrapper = new AppWrapper();
---
+++ project/modules/wrapper/ui/index.ts
@@ -0,0 +1,10 @@
+import { ReactiveModel } from '@beyond-js/reactive/model';
+
+class WrapperUI extends ReactiveModel<WrapperUI> {
+        
+}
+
+export /* bundle */ const wrapperUI = new WrapperUI();
+
+
+
